7aa313e99428a8121a60fa19aa9333e22c772568
Gaetano Esposito <g.esposito@swdes.intra>
2023-06-29 16:58:01 +0200

    full reset

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,10 +1,4 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = "catppuccin" }
-
-M.plugins = "custom.plugins"
-
-M.mappings = require "custom.mappings"
-
-return M
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {theme = 'catppuccin'}
+ return M
